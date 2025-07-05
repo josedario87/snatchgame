2b300851a9935a902c1e0729da99f2dc8d98646b
josedario87 <jodarioel87@gmail.com>
2025-07-05 15:07:59 -0600

    fix: Add missing exports for TradeOffer and TokenInventory in client types index

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,6 +1,8 @@
 // Re-export generated schema classes
 export { Player } from './Player'
 export { GameState } from './GameState'
+export { TradeOffer } from './TradeOffer'
+export { TokenInventory } from './TokenInventory'
 
 // Additional types that are not Schema classes
 export interface GameRoomOptions {
