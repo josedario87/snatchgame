d1707702731120a86f532ed273cb1a78285e6f4f
josedario87 <jodarioel87@gmail.com>
2025-08-15 12:41:11 -0600

    Remover mapeo de puertos, usar solo red principal para nginx

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
-# Iniciar el servidor Colyseus en background
+# Iniciar el servidor Colyseus en background (puerto 2567)
 echo "Starting Colyseus server..."
-cd /app/server && node dist/server/src/index.js &
+cd /app/server && PORT=2567 node dist/server/src/index.js &
 
 # Esperar un momento para que el servidor inicie
 sleep 2
 
-# Servir el cliente compilado
+# Servir el cliente compilado (puerto 3000)
 echo "Starting client server..."
-cd /app/client && serve -s dist -l 3000 &
+cd /app/client && serve -s dist -p 3000 &
 
 # Mantener el contenedor corriendo
 wait
